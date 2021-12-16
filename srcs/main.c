53f8704934852d8ee8aac899a11b14586162b7b4
FindingMiGo <straysheep_1727@yahoo.co.jp>
2021-12-16 12:46:46 +0900

    main処理を調整

--- srcs/main.c
+++ srcs/main.c
@@ -6,34 +6,36 @@
 /*   By: tisoya <tisoya@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 16:10:53 by tisoya            #+#    #+#             */
-/*   Updated: 2021/12/16 12:24:27 by tisoya           ###   ########.fr       */
+/*   Updated: 2021/12/16 12:44:08 by tisoya           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int args, char *argv[])
+void	size_branch(t_node *node_a, t_node *node_b)
 {
-	t_node	*node_a;
-	t_node	*node_b;
-	int		*sort;
 	size_t	size;
 
-	if (args < 2)
-		exit(1);
-	node_a = init_node(args, argv);
-	node_b = init_node(0, NULL);
 	size = node_a->val;
-
-	pre_sort(node_a, &sort);
-
 	if (is_sorted(node_a))
-		return (0);
+		return ;
 	if (size == 2)
 		case_two(node_a);
 	else if (size == 3)
 		case_three(node_a);
 	else if (size <= 6)
 		case_ngt_six(node_a, node_b);
+}
+
+int	main(int args, char *argv[])
+{
+	t_node	*node_a;
+	t_node	*node_b;
+	int		*sort;
+
+	if (args < 2)
+		exit(1);
+	node_a = init_node(args, argv);
+	node_b = init_node(0, NULL);
 	return (0);
 }
