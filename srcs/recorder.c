5002c10d25d066225f17c94733efea9ba9732e17
FindingMiGo <straysheep_1727@yahoo.co.jp>
2022-01-18 16:40:48 +0900

    記録処理を改善

--- srcs/recorder.c
+++ srcs/recorder.c
@@ -6,7 +6,7 @@
 /*   By: tisoya <tisoya@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 12:07:46 by tisoya            #+#    #+#             */
-/*   Updated: 2022/01/18 15:56:57 by tisoya           ###   ########.fr       */
+/*   Updated: 2022/01/18 16:40:05 by tisoya           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,6 +51,76 @@ size_t	offset_papb(char *record, size_t i, size_t j)
 	return (i);
 }
 
+size_t	integrate_rarb(char *record, size_t i, size_t j)
+{
+	if ((record[i] == 8 && record[j] == 9)
+		|| (record[i] == 9 && record[j] == 8))
+	{
+		record[i] = 1;
+		record[j] = 10;
+		return (0);
+	}
+	return (i);
+}
+
+size_t	integrate_rrarrb(char *record, size_t i, size_t j)
+{
+	if (record[j] == 7)
+	{
+		record[i] = 11;
+		record[j] = 1;
+		return (-1);
+	}
+	return (i);
+}
+
+size_t	offset_rrbrb(char *record, size_t i, size_t j)
+{
+	if ((record[i] == 7 && record[j] == 9)
+		|| (record[i] == 9 && record[j] == 7))
+	{
+		record[i] = 1;
+		record[j] = 1;
+		return (0);
+	}
+	return (i);
+}
+
+size_t	offset_rrara(char *record, size_t i, size_t j)
+{
+	if ((record[i] == 6 && record[j] == 8)
+		|| (record[i] == 8 && record[j] == 6))
+	{
+		record[i] = 1;
+		record[j] = 1;
+		return (0);
+	}
+	return (i);
+}
+
+size_t	integrate_and_offset(char *record, size_t i, size_t j)
+{
+	if ((record[i] == 4 && record[j] == 5)
+		|| (record[i] == 5 && record[j] == 4))
+		i = offset_papb(record, i, j);
+	else if ((record[i] == 8 && record[j] == 9)
+		|| (record[i] == 9 && record[j] == 8))
+		i = integrate_rarb(record, i, j);
+	else if ((record[i] == 7 && record[j] == 9)
+		|| (record[i] == 9 && record[j] == 7))
+		i = offset_rrbrb(record, i, j);
+	else if ((record[i] == 6 && record[j] == 8)
+		|| (record[i] == 8 && record[j] == 6))
+		i = offset_rrara(record, i, j);
+	else if (record[i] == 6)
+	{
+		while (record[j] == 6 || record[j] == 11 || record[j] == 1)
+			j++;
+		integrate_rrarrb(record, i, j);
+	}
+	return (i);
+}
+
 void	optimizer(char *record)
 {
 	size_t	i;
@@ -58,14 +128,19 @@ void	optimizer(char *record)
 
 	i = 0;
 	j = 1;
-	while (record[i])
+	// while (record[i])
+	// 	i++;
+	// i--;
+	// while (record[i] == 1)
+	// {
+	// 	record[i] = 0;
+	// 	i--;
+	// }
+	// i = 0;
+	while (record[i] != 0 && record[i] != 1)
 		i++;
-	i--;
-	while (record[i] == 1)
-	{
+	if (record[i] == 1)
 		record[i] = 0;
-		i--;
-	}
 	i = 0;
 	while (record[i + 1])
 	{
@@ -74,49 +149,33 @@ void	optimizer(char *record)
 		j = i + 1;
 		while (record[j] == 1)
 			j++;
-		if (record[i] == 4 || record[i] == 5)
-			i = offset_papb(record, i, j);
-		// if ((record[i] == 4 && record[j] == 5)
-		// 	|| (record[i] == 5 && record[j] == 4))
-		// {
-		// 	record[i] = 1;
-		// 	record[j] = 1;
-		// 	i = -1;
-		// }
-		else if ((record[i] == 8 && record[j] == 9)
-			|| (record[i] == 9 && record[j] == 8))
-		{
-			record[i] = 1;
-			record[j] = 10;
-			i = -1;
-		}
-		else if ((record[i] == 7 && record[j] == 9)
-			|| (record[i] == 9 && record[j] == 7))
-		{
-			record[i] = 1;
-			record[j] = 1;
-			i = -1;
-		}
-		else if ((record[i] == 6 && record[j] == 8)
-			|| (record[i] == 8 && record[j] == 6))
-		{
-			record[i] = 1;
-			record[j] = 1;
-			i = -1;
-		}
-		else if (record[i] == 6)
-		{
-			while (record[j] == 6 || record[j] == 11 || record[j] == 1)
-				j++;
-			if (record[j] == 7)
-			{
-				record[i] = 11;
-				record[j] = 1;
-				i = -1;
-			}
-		}
+		i = integrate_and_offset(record, i, j);
 		i++;
 	}
+}
+
+void	print_command(char c)
+{
+	if (c == 2)
+		write(1, "sa\n", 3);
+	else if (c == 3)
+		write(1, "sb\n", 3);
+	else if (c == 4)
+		write(1, "pb\n", 3);
+	else if (c == 5)
+		write(1, "pa\n", 3);
+	else if (c == 6)
+		write(1, "rra\n", 4);
+	else if (c == 7)
+		write(1, "rrb\n", 4);
+	else if (c == 8)
+		write(1, "ra\n", 3);
+	else if (c == 9)
+		write(1, "rb\n", 3);
+	else if (c == 10)
+		write(1, "rr\n", 3);
+	else if (c == 11)
+		write(1, "rrr\n", 4);
 }
 
 void	player(char *record)
@@ -129,26 +188,7 @@ void	player(char *record)
 	{
 		while (record[i] == 1)
 			i++;
-		if (record[i] == 2)
-			write(1, "sa\n", 3);
-		else if (record[i] == 3)
-			write(1, "sb\n", 3);
-		else if (record[i] == 4)
-			write(1, "pb\n", 3);
-		else if (record[i] == 5)
-			write(1, "pa\n", 3);
-		else if (record[i] == 6)
-			write(1, "rra\n", 4);
-		else if (record[i] == 7)
-			write(1, "rrb\n", 4);
-		else if (record[i] == 8)
-			write(1, "ra\n", 3);
-		else if (record[i] == 9)
-			write(1, "rb\n", 3);
-		else if (record[i] == 10)
-			write(1, "rr\n", 3);
-		else if (record[i] == 11)
-			write(1, "rrr\n", 4);
+		print_command(record[i]);
 		i++;
 	}
 }
